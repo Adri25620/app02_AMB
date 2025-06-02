bab6d3a22f184ea8cb068ecad39a679d7cb94494
Adri25620 <adrian25martinez@gmail.com>
2025-06-02 13:24:28 -0600

    layout

--- views/layout.php
+++ views/layout.php
@@ -1,73 +1,64 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="build/js/app.js"></script>
-    <link rel="shortcut icon" href="<?= asset('images/cit.png') ?>" type="image/x-icon">
-    <link rel="stylesheet" href="<?= asset('build/styles.css') ?>">
-    <title>DemoApp</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="<?= asset('build/js/app.js') ?>"></script>
+  <link rel="shortcut icon" href="<?= asset('images/cit.png') ?>" type="image/x-icon">
+  <link rel="stylesheet" href="<?= asset('build/styles.css') ?>">
+  <title>DemoApp</title>
+
+  <style>
+    .navbar-nav .nav-link {
+      font-weight: 800;
+    }
+  </style>
 </head>
+
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark  bg-dark">
-        
-        <div class="container-fluid">
+  <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+    <div class="container-fluid">
 
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <a class="navbar-brand" href="/ejemplo/">
-                <img src="<?= asset('./images/cit.png') ?>" width="35px'" alt="cit" >
-                Aplicaciones
-            </a>
-            <div class="collapse navbar-collapse" id="navbarToggler">
-                
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="margin: 0;">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="/ejemplo/"><i class="bi bi-house-fill me-2"></i>Inicio</a>
-                    </li>
-  
-                    <div class="nav-item dropdown " >
-                        <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">
-                            <i class="bi bi-gear me-2"></i>Dropdown
-                        </a>
-                        <ul class="dropdown-menu  dropdown-menu-dark "id="dropwdownRevision" style="margin: 0;">
-                            <!-- <h6 class="dropdown-header">Información</h6> -->
-                            <li>
-                                <a class="dropdown-item nav-link text-white " href="/aplicaciones/nueva"><i class="ms-lg-0 ms-2 bi bi-plus-circle me-2"></i>Subitem</a>
-                            </li>
-                        
-                    
-                        
-                        </ul>
-                    </div> 
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item me-4">
+            <a class="nav-link active" aria-current="page" href="/app02_AMB/"><i class="bi bi-house"> INICIO</i></a>
+          </li>
+          <li class="nav-item me-4">
+            <a class="nav-link" href="/app02_AMB/clientes"><i class="bi bi-person"> CLIENTES</i></a>
+          </li>
+          <li class="nav-item me-4">
+            <a class="nav-link" href="/app02_AMB/productos"><i class="bi bi-archive"> PRODUCTOS</i></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/app02_AMB/venta"><i class="bi bi-cart-dash"> VENTA</i></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 
-                </ul> 
-                <div class="col-lg-1 d-grid mb-lg-0 mb-2">
-                    <!-- Ruta relativa desde el archivo donde se incluye menu.php -->
-                    <a href="/menu/" class="btn btn-danger"><i class="bi bi-arrow-bar-left"></i>MENÚ</a>
-                </div>
 
-            
-            </div>
-        </div>
-        
-    </nav>
-    <div class="progress fixed-bottom" style="height: 6px;">
-        <div class="progress-bar progress-bar-animated bg-danger" id="bar" role="progressbar" aria-valuemin="0" aria-valuemax="100"></div>
+  <div class="progress fixed-bottom" style="height: 6px;">
+    <div class="progress-bar progress-bar-animated bg-danger" id="bar" role="progressbar" aria-valuemin="0" aria-valuemax="100"></div>
+  </div>
+  <div class="container-fluid pt-5 mb-4" style="min-height: 85vh">
+
+    <?php echo $contenido; ?>
+  </div>
+  <div class="container-fluid ">
+    <div class="row justify-content-center text-center">
+      <div class="col-12">
+        <p style="font-size:xx-small; font-weight: bold;">
+          Subteniente Caballería Adrian Martínez Barrientos, <?= date('Y') ?> &copy;
+        </p>
+      </div>
     </div>
-    <div class="container-fluid pt-5 mb-4" style="min-height: 85vh">
-        
-        <?php echo $contenido; ?>
-    </div>
-    <div class="container-fluid " >
-        <div class="row justify-content-center text-center">
-            <div class="col-12">
-                <p style="font-size:xx-small; font-weight: bold;">
-                        Comando de Informática y Tecnología, <?= date('Y') ?> &copy;
-                </p>
-            </div>
-        </div>
-    </div>
+  </div>
 </body>
+
 </html>
